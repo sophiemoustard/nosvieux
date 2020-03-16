828f68b0be22ee45c8cfcd09a680ba9fd2bd7336
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-16 11:10:56 +0100

    Design

--- components/NosVieux.vue
+++ components/NosVieux.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="bloc">
-    <div class="hashtag center">
-      <div class="blue-text">#</div>
-      <div class="green-text">Nos</div>
-      <div class="green-blue-text">Vi</div>
-      <div class="blue-text">eux</div>
+    <div class="hashtag-container">
+      <div class="hashtag">
+        <div class="blue-text">#</div>
+        <div class="green-text">Nos</div>
+        <div class="green-blue-text">Vi</div>
+        <div class="blue-text">eux</div>
+      </div>
     </div>
-    <div class="center dark-blue-text">
+    <div class="center dark-blue-text description">
       « Nous changeons nos habitudes pour protéger nos aînés et les personnes
       vulnérables. Mais confinement ne doit pas rimer avec isolement : pas de
       poignées de main, faisons plus de création de liens! »
     </div>
-    <div class="green-background">
-      Lavons nous les mains mais ne nous tournons pas les pouces
+    <div class="green-background punchline">
+      Lavons nous les mains mais ne nous tournons pas les pouces !
     </div>
   </div>
 </template>
 
 <style scoped>
+.hashtag-container {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+}
 .hashtag {
   font-size: 42px;
   line-height: 57px;
   display: flex;
+}
+.description {
+  margin-bottom: 40px;
+}
+.punchline {
+  color: white;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 27px;
+  padding: 8px 4px;
+  margin-bottom: 40px;
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <nuxt />
   </div>
 </template>
