b1d6bd64207457848065be1c434183ca49601d4b
Chloe Molle <chloe.molle@neuf.fr>
2020-03-16 11:38:08 +0100

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bloc blue-background footer-container">
+    <div>#NosVieux 2020</div>
+    <div>Mentions Légales</div>
+    <div>Contact</div>
+  </div>
+</template>
+
+<style scoped>
+.footer-container {
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  color: white;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,19 @@
   <div class="container">
     <nos-vieux />
     <act />
+    <ni-footer />
   </div>
 </template>
 
 <script>
 import Act from '~/components/Act'
+import NiFooter from '~/components/Footer'
 import NosVieux from '~/components/NosVieux'
 
 export default {
   components: {
     Act,
+    NiFooter,
     NosVieux
   }
 }
