9bdf2c9fba39d4f42c27c1c9e176f614366e984c
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-16 10:18:20 +0100

    Add text in nosvieux components

--- components/NosVieux.vue
+++ components/NosVieux.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>Lavons nous les mains mais ne nous tournons pas les pouces</div>
+  <div class="bloc">
+    <div class="hashtag center">
+      <div class="blue-text">#</div>
+      <div class="green-text">Nos</div>
+      <div class="green-blue-text">Vi</div>
+      <div class="blue-text">eux</div>
+    </div>
+    <div class="center dark-blue-text">
+      « Nous changeons nos habitudes pour protéger nos aînés et les personnes
+      vulnérables. Mais confinement ne doit pas rimer avec isolement : pas de
+      poignées de main, faisons plus de création de liens! »
+    </div>
+    <div class="green-background">
+      Lavons nous les mains mais ne nous tournons pas les pouces
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.hashtag {
+  font-size: 42px;
+  line-height: 57px;
+  display: flex;
+}
+</style>
