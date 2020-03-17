9bab18b14af763b140b89f1506a009ecc6ce2b87
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-17 13:26:13 +0100

    Add idea profile page

---
+++ pages/idea/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ni-header />
+  </div>
+</template>
+
+<script>
+import NiHeader from '~/components/header'
+
+export default {
+  name: 'IdeaProfile',
+  components: {
+    NiHeader
+  },
+  data() {
+    return {
+      id: this.$route.params.id
+    }
+  }
+}
+</script>
+
+<style scoped></style>
