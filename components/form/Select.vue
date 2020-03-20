4fe58e57a572c04e825914393153e22fea421b3f
jctrebalag <jctrebalag@gmail.com>
2020-03-20 15:15:49 +0100

    NV-8 Add select component

---
+++ components/form/Select.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="select">
+    <select :value="value" @input="$emit('input', $event.target.value)">
+      <option value="">...</option>
+      <option
+        v-for="(option, index) in options"
+        :key="index"
+        :value="option.value"
+      >
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Select',
+  props: {
+    value: { type: String, default: '' },
+    options: { type: Array, default: () => [] }
+  }
+}
+</script>
